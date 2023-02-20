8a6e037f930c072d1badeb4f9d4302d1d19bc8a7
TusharPatwal <patwaltushar001@gmail.com>
2023-02-20 23:31:44 +0530

    java strings functions

---
+++ java-01/day04_p.java
@@ -0,0 +1,22 @@
+import java.util.*;
+// java strings functions 
+class day04_p{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = "TusharPatwal";
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.indexOf('r'));
+        
+        
+        
+        
+        
+        
+        
+        
+        sc.close();
+    }
+}
