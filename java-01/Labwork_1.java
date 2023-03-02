82a78a023fb05ced664761029c88920bc30f4d96
TusharPatwal <patwaltushar001@gmail.com>
2023-03-02 08:23:13 +0530

    format document

--- java-01/Labwork_1.java
+++ java-01/Labwork_1.java
@@ -44,21 +44,21 @@ class Labwork_1 {
          */
         Scanner sc = new Scanner(System.in);
 
-        //Q7 wap that except your name, age, and percentage and display.
+        // Q7 wap that except your name, age, and percentage and display.
         String name = sc.nextLine();
         int age = sc.nextInt();
         float percentage = sc.nextFloat();
 
         System.out.println("My name is " + name + " and age is " + age + " and percentage is " + percentage);
 
-        //Q8 wap that display all even number between 1 to 100.
+        // Q8 wap that display all even number between 1 to 100.
         for (int i = 1; i < 101; i++) {
             if (i % 2 == 0) {
                 System.out.print(i + " ");
             }
         }
 
-        //Q9 wap that excepts the day of weeks and displays the same.
+        // Q9 wap that excepts the day of weeks and displays the same.
         int day = sc.nextInt();
         switch (day) {
             case 1:
@@ -86,7 +86,7 @@ class Labwork_1 {
                 System.out.println("Invalid day");
         }
 
-        //Q10 wap that checks weather the given number is prime or not.
+        // Q10 wap that checks weather the given number is prime or not.
         int num = sc.nextInt();
         boolean flag = false;
         for (int i = 2; i <= num / 2; ++i) {
@@ -100,16 +100,15 @@ class Labwork_1 {
         else
             System.out.println(num + " is not a prime number.");
 
-            
-        //Q11 wap that checks weather num is divisible by 3 or 7.
+        // Q11 wap that checks weather num is divisible by 3 or 7.
         int num1 = sc.nextInt();
-        if(num1%3==0 || num1%7==0){
+        if (num1 % 3 == 0 || num1 % 7 == 0) {
             System.out.println("Number is divisible 3 or 7");
-        }else{
+        } else {
             System.out.println("Number is not divisible 3 or 7");
         }
 
-        //Q12 wap that excepts grade from user and display result a,b,c,d.
+        // Q12 wap that excepts grade from user and display result a,b,c,d.
         String grade = sc.next();
         switch (grade) {
             case "a":
