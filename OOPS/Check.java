6fa2d01e96d31cf42a6a1c4b7666312157c660eb
TusharPatwal <patwaltushar001@gmail.com>
2023-03-29 06:37:51 +0530

    interface variable in java

---
+++ OOPS/Check.java
@@ -0,0 +1,20 @@
+//                      Interface Variables 
+
+interface costomerAnsh{
+    int amt = 5; // public + static + final
+    void purchase(); // public + abstract
+}
+class sellerTushar implements costomerAnsh{
+    @Override 
+    public void purchase(){
+        System.out.println("Tushar needs " + amt + " kg rice.");
+    }
+}
+class Check{
+    public static void main(String[] args) {
+        
+        costomerAnsh obj = new sellerTushar();
+        obj.purchase();
+        System.out.println(costomerAnsh.amt);
+    }
+}
