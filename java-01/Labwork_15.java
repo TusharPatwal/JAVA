a12e2d89535f866faf07b9fa049ae251971661cd
TusharPatwal <patwaltushar001@gmail.com>
2023-04-18 23:41:49 +0530

    q13
    13. Consider a university where students who participate in the national games or Olympics are given some grace marks. Therefore, the final marks awarded = Exam_Marks + Sports_Grace_Marks. A class diagram representing this scenario is as follow;

--- java-01/Labwork_15.java
+++ java-01/Labwork_15.java
@@ -1,5 +1,7 @@
 // 13. Consider a university where students who participate in the national games or Olympics are given some grace marks. Therefore, the final marks awarded = Exam_Marks + Sports_Grace_Marks. A class diagram representing this scenario is as follow;
 
+import java.util.Scanner;
+
 class Student {
     String Name;
     long rollNo;
@@ -37,8 +39,50 @@ class Result extends Exam implements Sports {
 public class Labwork_15 {
     public static void main(String[] args) {
         Result r1 = new Result();
-        r1.getData("Tushar", 98, 100);
+        Scanner sc = new Scanner(System.in);
+        String Name = sc.next();
+        int rollNo = sc.nextInt();
+        int Exam_Marks = sc.nextInt();````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        r1.getData(Name, rollNo, Exam_Marks);
         System.out.println(r1.resultExam());
+        sc.close();
     }
 
 }
