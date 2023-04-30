36baa3f8bdd9f60aa765b9997389d881d809f824
TusharPatwal <patwaltushar001@gmail.com>
2023-04-30 23:06:03 +0530

    throws keyword

---
+++ OOPS/T.java
@@ -0,0 +1,8 @@
+public class T {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+    }
+}
